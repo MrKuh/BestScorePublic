3956ddf09649afa5ef7b0fed1eee61bb672340fd
Benefitzer <renbea18@htl-kaindorf.at>
2021-04-28 19:03:59 +0200

    Teammate

--- app/src/main/java/project/bestscore/ui/teammates/TeammateAdapter.java
+++ app/src/main/java/project/bestscore/ui/teammates/TeammateAdapter.java
@@ -1,4 +1,37 @@
 package project.bestscore.ui.teammates;
 
-public class TeammateAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import project.bestscore.R;
+
+public class TeammateAdapter extends RecyclerView.Adapter<TeammateViewholder> {
+
+
+
+
+    @NonNull
+    @Override
+    public TeammateViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_teammates_item,parent,false);
+        TextView tvNameOfPlayer = view.findViewById(R.id.tvNameOfPlayer);
+        TextView tvWinsOfPlayer = view.findViewById(R.id.tvWinsOfPlayer);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TeammateViewholder holder, int position) {
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
--- app/src/main/java/project/bestscore/ui/teammates/TeammateViewholder.java
+++ app/src/main/java/project/bestscore/ui/teammates/TeammateViewholder.java
@@ -1,5 +1,29 @@
 package project.bestscore.ui.teammates;
 
-public class TeammateViewholder {
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TeammateViewholder extends RecyclerView.ViewHolder {
+    private TextView tvNameOfPlayer;
+    private TextView tvWinsOfPlayer;
+
+
+    public TeammateViewholder(@NonNull View itemView, Activity activity, TeammateAdapter adapter, Context context, TextView tvNameOfPlayer, TextView tvWinsOfPlayer) {
+        super(itemView);
+        this.tvNameOfPlayer = tvNameOfPlayer;
+        this.tvWinsOfPlayer = tvWinsOfPlayer;
+    }
+
+    public TextView getTvNameOfPlayer() {
+        return tvNameOfPlayer;
+    }
+
+    public TextView getTvWinsOfPlayer() {
+        return tvWinsOfPlayer;
+    }
 }
